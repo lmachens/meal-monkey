f801cfd3a816b4bda5aaea0273de9ee19e004ac8
Leon Machens <leon@machens.koeln>
2021-01-25 13:47:50 +0100

    Initial commit

---
+++ src/components/avatar/avatar.stories.js
@@ -0,0 +1,9 @@
+import "./avatar.css";
+import avatar from "./avatar.html";
+
+export default {
+  title: "Components/Avatar",
+  parameters: { layout: "centered" },
+};
+
+export const dog = () => avatar;
---
+++ src/components/typography/typography.stories.js
@@ -0,0 +1,9 @@
+import "./typography.css";
+import typography from "./typography.html";
+
+export default {
+  title: "Components/Typography",
+  parameters: { layout: "centered" },
+};
+
+export const headers = () => typography;
