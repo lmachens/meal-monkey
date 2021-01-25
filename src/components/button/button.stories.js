29febb922ab39e36552a239024536c3274c8cd64
lmachens <leon@machens.koeln>
2021-01-25 14:10:00 +0100

    Add button component

--- src/components/button/button.stories.js
+++ src/components/button/button.stories.js
@@ -1,11 +1,11 @@
 import "./button.css";
-import buttonText from "./button-text.html";
-import buttonLogo from "./button-logo.html";
+import buttonBorder from "./button-border.html";
+import buttonFilled from "./button-filled.html";
 
 export default {
   title: "Components/Button",
   parameters: { layout: "centered" },
 };
 
-export const withText = () => buttonText;
-export const withLogo = () => buttonLogo;
+export const border = () => buttonBorder;
+export const filled = () => buttonFilled;
