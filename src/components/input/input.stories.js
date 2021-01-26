6e2b642187c854a0d1e10c1d5351c82ce88df5cf
lmachens <leon@machens.koeln>
2021-01-26 14:10:08 +0100

    Add password and number stories

--- src/components/input/input.stories.js
+++ src/components/input/input.stories.js
@@ -1,6 +1,8 @@
 import "./input.css";
 import inputEmpty from "./input-empty.html";
 import inputEmail from "./input-email.html";
+import inputPassword from "./input-password.html";
+import inputNumber from "./input-number.html";
 
 export default {
   title: "Components/Input",
@@ -9,3 +11,5 @@ export default {
 
 export const empty = () => inputEmpty;
 export const email = () => inputEmail;
+export const password = () => inputPassword;
+export const number = () => inputNumber;
