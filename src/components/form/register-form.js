9bb92218c1ce41808da08dad4f3adde5ca4225a1
lmachens <leon@machens.koeln>
2021-02-01 15:47:56 +0100

    Add register button

--- src/components/form/register-form.js
+++ src/components/form/register-form.js
@@ -1,10 +1,22 @@
+/*
+<form class="form">
+    <h2>Reset Password</h2>
+    <p>Please enter your email to receive a link to create a new password via email</p>
+    <input placeholder="email" />
+    <button>Register</button>
+</form>
+*/
 export function createRegisterForm() {
-  return `
-      <form class="form">
-        <h2>Reset Password</h2>
-        <p>Please enter your email to receive a link to create a new password via email</p>
-        <input placeholder="email" />
-        <input type="submit" />
-      </form>
-    `;
+  //   const title = document.createElement("h2");
+  //   title.innerText = "Reset Password";
+  const button = document.createElement("button");
+  button.innerText = "Register";
+
+  function register() {
+    alert("Registered");
+  }
+
+  button.addEventListener("click", register);
+
+  return button;
 }
