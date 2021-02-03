42a4e936a24212e1387032cce4423916926b74bd
phlgr <philippm.gartz@gmail.com>
2021-02-03 15:31:27 +0100

    💡 Explain createElement in depth

--- src/utils/createElement.js
+++ src/utils/createElement.js
@@ -1,6 +1,16 @@
 export function createElement(tagName, props) {
   const element = document.createElement(tagName);
   Object.assign(element, props);
+  /*
+  props is a completely freely assignable name, it could also be named fische
+  
+  Object.assign(element, props) replaces this chunk of code:
+  element.placeholder = placeholder;
+  element.type = type;
+  element.innerText = innerText;
+  element.className = className;
+  element.href = href;
+  */
 
   return element;
 }
